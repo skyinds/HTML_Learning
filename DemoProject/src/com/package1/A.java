6236a3a75574874cf4c2d65ed971f3cf3678bf8e
SidKarthik1437 <SidKarthik1437@gmail.com>
2024-09-26 11:05:38 +0530

    changes updated

--- DemoProject/src/com/package1/A.java
+++ DemoProject/src/com/package1/A.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 public class A {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println(" main method ");
 		try {
 
@@ -17,17 +16,6 @@ public class A {
 		} catch (Exception e) {
 			System.out.println("Exception caught 1:");
 		}
-=======
-	System.out.println(" pusing the file  ");
-		  try {
-			  
-			  int result = 10/ 0; 	
-			 System.out.println("Result: " + result); 
-			 
-		   } catch (Exception e) {
-			 System.out.println("Exception caught 1:");
-		   }  
->>>>>>> 04741048988a600596b2351396e4a9a90c81902e
 
 	}
 
