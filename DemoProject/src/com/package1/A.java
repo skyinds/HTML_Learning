72e0c34e1176856c2a2a3661f4dfda1159e5bd2a
Hp <Hp@Samskruthi>
2024-09-26 11:35:43 +0530

    message added

--- DemoProject/src/com/package1/A.java
+++ DemoProject/src/com/package1/A.java
@@ -8,7 +8,7 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println(" main method ");
-		System.out.println(" test pushed...");
+		
 		try {
 
 			int result = 10 / 0;
