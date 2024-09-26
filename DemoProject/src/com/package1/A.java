54821de7b29cb4a007893dad60459707ab1262cd
SidKarthik1437 <SidKarthik1437@gmail.com>
2024-09-26 10:32:34 +0530

    Updates

--- DemoProject/src/com/package1/A.java
+++ DemoProject/src/com/package1/A.java
@@ -4,24 +4,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class A{
+public class A {
 
-	
 	public static void main(String[] args) {
-	System.out.println(" main method ");
-		  try {
-			  
-			  int result = 10/ 0; 	
-			 System.out.println("Result: " + result); 
-			 
-		   } catch (Exception e) {
-			 System.out.println("Exception caught 1:");
-		   }  
+		System.out.println(" main method ");
+		System.out.println("Updated");
+		try {
 
-		
+			int result = 10 / 0;
+			System.out.println("Result: " + result);
+
+		} catch (Exception e) {
+			System.out.println("Exception caught 1:");
+		}
+
 	}
-	
 
 }
-	
-
