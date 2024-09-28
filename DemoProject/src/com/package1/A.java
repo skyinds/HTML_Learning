1bb66c2bd9ffa0c83d932112f09fd9c74d5b5f37
PC <PC@sky-pc>
2024-09-28 06:46:08 +0200

    updated

--- DemoProject/src/com/package1/A.java
+++ DemoProject/src/com/package1/A.java
@@ -7,8 +7,7 @@ import java.io.IOException;
 public class A {
 
 	public static void main(String[] args) {
-		System.out.println(" main method ");
-		System.out.println("local branch");
+
 		
 		try {
 
